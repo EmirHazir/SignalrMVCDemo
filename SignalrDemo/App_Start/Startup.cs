38529b78ffb539f89e3cd39189285a0feacb37bd
EmirHazir <emircanaziz@gmail.com>
2017-12-28 00:38:44 +0300

    basic-chat

---
+++ SignalrDemo/App_Start/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalrDemo.App_Start.Startup))]
+
+namespace SignalrDemo.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
---
+++ SignalrDemo/Hubs/ChatHub.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalrDemo.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        //burada önemli olan isimlerin eşleşmesi değil parametrelerin SIRASI
+        public void SendMessage(string name, string message)
+        {
+
+            Clients.Others.GetMessageOther(name,message);
+
+            Clients.Caller.GetMessageCaller(message);
+
+
+
+            //Bütün clientlara tetiklensin
+            //Clients.All()
+            
+            //Diğer clientslara
+            //Clients.Others()
+
+            //ben bunu sunucuya gönderim yine kendime gönderim yapıyorum
+            //Clients.Caller()
+
+            //sadece belli bir clienta gönder
+            //Clients.Users("Emir").doWork()
+
+            //ID ye gönder
+            //Clients.User(userId:1).doWork()
+
+        }
+    }
+}
